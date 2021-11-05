7f2bee6d8c548d1a6bba0b4431ba31303e092523
Dewa <73357308+DewaTriWijaya@users.noreply.github.com>
2021-11-05 23:53:41 +0700

    Add project to github

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float Health = 50f;
+
+    public UnityAction<GameObject> OnEnemyDestroyed = delegate { };
+
+    private bool _isHit = false;
+
+    void OnDestroy()
+    {
+        if (_isHit)
+        {
+            OnEnemyDestroyed(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Rigidbody2D>() == null) return;
+
+        if(collision.gameObject.tag == "Bird")
+        {
+            _isHit = true;
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.tag == "Obstacle")
+        {
+            // hitung damage yg diperoleh
+            float damage = collision.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 10;
+            Health -= damage;
+
+            if(Health <= 0)
+            {
+                _isHit = true;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
